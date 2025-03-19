a228dbf8db86b9997b35697ac402b250c6d72af6
孙达明 <shun_@outlook.com>
2025-03-19 14:56:56 +0800

    feat:去除多余的注解

--- justsimple-core/src/main/java/org/mutantcat/justsimple/Application.java
+++ justsimple-core/src/main/java/org/mutantcat/justsimple/Application.java
@@ -1,9 +1,7 @@
 package org.mutantcat.justsimple;
 
-import org.mutantcat.justsimple.annotation.JustSimple;
 import org.mutantcat.justsimple.starter.ApplicationStarter;
 
-@JustSimple(packageName = "org.mutantcat.justsimple")
 public class Application {
     public static void start(Class<?> clazz,String[] args) {
         ApplicationStarter starter = new ApplicationStarter();
